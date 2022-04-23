ea495e8657d071f69da9a86b2494e6d9072e0510
Ben Diehl <diehlb@gmail.com>
2022-04-23 16:41:32 -0500

    Add analyze endpoint

--- api/main.py
+++ api/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
 
 app = FastAPI()
+
+class PredictionRequest(BaseModel):
+  query_string: str
 
 @app.get("/health")
 async def root():
     return {"message": "This is live"}
+
+@app.post("/analyze")
+def analyze(request: PredictionRequest):
+    sentiment_model = pipeline("sentiment-analysis")
+
+    sentiment_query_sentence = request.query_string
+    sentiment = sentiment_model(sentiment_query_sentence)
+    print(f"Sentiment test: {sentiment_query_sentence} == {sentiment}")
