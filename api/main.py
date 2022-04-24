1babc8434df72929838b58ce18a164ef6b66f542
Ben Diehl <diehlb@gmail.com>
2022-04-24 15:17:53 -0500

    🔎 errors and logs

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import pipeline
 
@@ -10,15 +10,37 @@ app = FastAPI()
 class PredictionRequest(BaseModel):
   query_string: str
 
-@app.get("/health")
+@app.get('/health')
 async def root():
     logger.info("Health was queried")
-    return {"message": "App is running"}
+    return {'message': "App is running"}
 
-@app.post("/analyze")
+@app.post('/analyze')
 def analyze(request: PredictionRequest):
-    sentiment_model = pipeline("sentiment-analysis")
+    sentiment_model = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
-    sentiment_query_sentence = request.query_string
-    sentiment = sentiment_model(sentiment_query_sentence)
-    print(f"Sentiment test: {sentiment_query_sentence} == {sentiment}")
+    response = {'query':request.query_string}
+    try:
+        model_out = sentiment_model(response['query'])[0]
+        #Force error
+        #raise ValueError("Could not understand comment")
+    except BaseException as e: # Catch everything
+        err_str = f"Model analysis failed ({type(e).__name__}): {str(e)}"
+        logger.error(err_str)
+        raise HTTPException(status_code=500, detail=err_str)
+
+    response.update(**model_out)
+
+    logger.info(f"Analyzed => {response['label'].lower()}")
+    return response
+
+
+##############
+# Error overrides
+
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import request_validation_exception_handler
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    logger.error(f"{request['path']} Invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
